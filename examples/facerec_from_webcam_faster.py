33f9ed84baa6ba8c7603f48893d84d3cc2e49d4b
Harshad Mahadik <harshad@arcturusnetworks.com>
2019-10-09 08:32:07 -0400

    Updated facerec from webcam faster example to detect Harshad

--- examples/facerec_from_webcam_faster.py
+++ examples/facerec_from_webcam_faster.py
@@ -22,14 +22,19 @@ obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 biden_image = face_recognition.load_image_file("biden.jpg")
 biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
+harshad_image = face_recognition.load_image_file("Harshad.jpg")
+harshad_face_encoding = face_recognition.face_encodings(harshad_image)[0]
+
 # Create arrays of known face encodings and their names
 known_face_encodings = [
     obama_face_encoding,
-    biden_face_encoding
+    biden_face_encoding,
+    harshad_face_encoding
 ]
 known_face_names = [
     "Barack Obama",
-    "Joe Biden"
+    "Joe Biden",
+    "Harshad"
 ]
 
 # Initialize some variables
